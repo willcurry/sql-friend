341594f19bbe85f16eebbd15425f5ca282418df7
Will Curry <watercrafthd@gmail.com>
2017-02-10 16:09:37 +0000

    Check for invalid usage

--- lib/database.rb
+++ lib/database.rb
@@ -6,24 +6,35 @@ class Database
   end
 
   def connect
-    @connection = PG::Connection.open(:dbname => @database_name)
-    if @connection
-      return "Connected"
-    else
+    begin
+      @connection = PG::Connection.open(:dbname => @database_name)
+    rescue
       return "Failed to connect"
     end
+    return "Connected"
   end
 
   def query(query)
-    result = @connection.exec_params(query)
-    return format(result)
+    connect
+    data = send(query)
+    disconnect
+    data
   end
 
-  def close_connection
+  def disconnect
     @connection.finish
   end
 
   private
+
+  def send(query)
+    begin
+      result = @connection.exec_params(query)
+      format(result)
+    rescue
+      "Invalid query"
+    end
+  end
 
   def format(result)
     data = []
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -5,17 +5,23 @@ RSpec.describe Database do
     @database = Database.new('willcurry')
   end
 
-  after(:each) do
-    @database.close_connection
-  end
-
   it "connects successfully" do
     expect(@database.connect).to eq("Connected")
   end
 
+  it "lets us know if it did not connect" do
+    invalid_database = Database.new('invalid_name_of_database')
+    expect(invalid_database.connect).to eq('Failed to connect')
+  end
+
   it "gets correct results from database" do
-    @database.connect
     result = @database.query('SELECT * FROM test;')
     expect(result.first["name"]).to eq("Will")
   end
+
+  it "lets us know if there was an issue with the query" do
+    result = @database.query('this is an invalid query')
+    expect(result).to eq("Invalid query")
+  end
 end
+
